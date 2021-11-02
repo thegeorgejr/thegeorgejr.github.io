7133856852bc3bd14e577386069193ace279cbe6
dayumsam <dayumsam22@gmail.com>
2021-11-03 01:37:00 +0530

    added index page anim

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    let heroContent = ["Footwear Designer", "UI/UX Designer"]
+
+    var typed = new Typed("#designation .placeholder", {
+        strings: heroContent,
+        typeSpeed: 80,
+        backSpeed: 60,
+        backDelay: 700,
+        showCursor: false,
+        loop: true,
+      });
+
+});
